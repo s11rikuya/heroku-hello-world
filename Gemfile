12ff06823ab8995c34cdbbab4840438fd7f67393
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-22 18:09:43 +0900

    facebook test

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
-source :rubygems
+# frozen_string_literal: true
+
+source "https://rubygems.org"
 ruby '2.4.2'
+gem 'dotenv'
+gem 'koala'
+gem 'omniauth-facebook'
 gem 'sinatra'
+gem 'sinatra-contrib'
--- app.rb
+++ app.rb
@@ -1,5 +1,42 @@
-require 'rubygems'
 require 'sinatra'
-get '/' do
-  'HerokuでSinatraを使ってHello world!'
+require 'sinatra/reloader'
+require 'omniauth-facebook'
+require 'net/http'
+require 'json'
+require 'time'
+require 'dotenv'
+require 'pp'
+enable :sessions
+Dotenv.load
+
+APP_SECRET = ENV['APP_SECRET']
+APP_ID = ENV['APP_ID']
+
+class SinatraOmniAuth < Sinatra::Base
+  configure do
+    set :sessions, true
+    set :inline_templates, true
+  end
+
+  use OmniAuth::Builder do
+    provider :facebook, APP_ID, APP_SECRET,
+    scope: "email, user_birthday, public_profile, user_posts", display: "popup",
+    info_fields: "email, birthday, gender, first_name, last_name, posts"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/auth/:provider/callback' do
+    @provider = params[:provider]
+    @result = request.env['omniauth.auth']
+    session[:access_token] = @result['credentials']['token']
+    redirect '/index'
+  end
+
+  get '/index' do
+    "#{session[:access_token]}"
+  end
 end
+SinatraOmniAuth.run! if __FILE__ == $0
